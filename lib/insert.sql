217dbe67b61c02a8f0fab939d6f15da103c6d9d6
dnj547 <dnj547@live.com>
2019-04-09 15:14:03 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES
+(1, 'Mr. Chocolate', 40, 'Male', 'brown', 'grumpy', 1),
+(2, 'Rowdy', 4, 'Male', 'black', 'rambunctious', 1),
+(3, 'Tabitha', 10, 'Female', 'brown', 'quiet', 1),
+(4, 'Sergeant Brown', 70, 'Male', 'brown', 'old', 0),
+(5, 'Melissa', 30, 'Female', 'black', 'kind', 1),
+(6, 'Grinch', 33, 'Male', 'green', 'rude', 1),
+(7, 'Wendy', 12, 'Female', 'brown', 'opinionated', 1),
+(8, null, 28, 'Unknown', 'red', 'killer', 0);
